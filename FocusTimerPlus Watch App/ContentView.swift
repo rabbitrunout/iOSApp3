21f6318fe81cfc1534355b4bbff9645c56a2f2a7
Irina Saf <irina.safronova0801@gmail.com>
2025-10-22 10:53:14 -0400

    Initial Commit

---
+++ FocusTimerPlus Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusTimerPlus Watch App
+//
+//  Created by Irina Saf on 2025-10-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusTimerPlus Watch App/FocusTimerPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerPlusApp.swift
+//  FocusTimerPlus Watch App
+//
+//  Created by Irina Saf on 2025-10-22.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerPlus_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
